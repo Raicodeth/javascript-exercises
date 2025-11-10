6d90ec0fd3f8643f939df33ec0fda37997eec3f6
Raicodeth <190210161+Raicodeth@users.noreply.github.com>
2025-11-10 19:33:32 +0300

    Checking through exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let repeated = ('');
+    while ((repeated.length) != (string.length * num)) {
+       repeated = repeated.concat(string);
+    if (num <= 0) {
+        return 'ERROR'
+    }
+    }
+return repeated
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    arrd = string.split('').toReversed('').join('');
+    console.log(arrd);
+    return arrd;}
 
-};
 
 // Do not edit below this line
 module.exports = reverseString;
