3193786704fd78520c9fe80021daff5b0d261273
Raicodeth <190210161+Raicodeth@users.noreply.github.com>
2025-11-12 23:01:08 +0300

    finish removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+function removeFromArray(arr) {
+    const args = Array.prototype.slice.call(arguments, 1);
+    let filtered = [];
+    for (ele of arr) {
+        if (!(args.includes(ele))) {
+            filtered.push(ele);            
+        }        
+    }
+    return filtered;              
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
