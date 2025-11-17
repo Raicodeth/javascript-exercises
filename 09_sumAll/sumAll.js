8ebd6f616663f3572cf0c656013f2cdb669c7a63
Raicodeth <190210161+Raicodeth@users.noreply.github.com>
2025-11-17 21:45:30 +0300

    Handle sum all exercise Iterate through alternatives

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+function sumAll(a, b) {
+if ( !Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0 ) {   
+    return 'ERROR';
+}
+else {
+    let arred = [a,b];   
+    let sorted = arred.toSorted((x, y) => x-y);  
+    
+    let ranged = []
+    for (let i=(sorted[0]); i<=sorted[1]; i++) {        
+        ranged.push(i);
+    }    
+    
+    let summed;    
+    summed = ranged.reduce(function(acc, curr) {
+        return (acc + curr)
+    }) 
+    return summed;}      
+}
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
