f4586c05b5eaada4e8c6d7b461a3bd1a3b8419ae
Raicodeth <190210161+Raicodeth@users.noreply.github.com>
2025-11-22 21:07:37 +0300

    finish leapYears exercise finish tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
-};
+function convertToCelsius(Fnumber) {
+    const Celed = Math.round(((Fnumber - 32) * 5/9) * 10) / 10;
+    return Celed;
+}
 
-const convertToFahrenheit = function() {
-};
+function convertToFahrenheit(Cnumber) {
+    const Fahed = Math.round((Cnumber * 9/5 + 32) * 10) / 10;
+    return Fahed;
+}
+
 
 // Do not edit below this line
 module.exports = {
